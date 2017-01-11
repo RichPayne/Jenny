13c1f09aa541582ea019c7a7f60fab3bd47c768d
Richard Payne <richardncpayne@gmail.com>
2017-01-11 13:36:52 +0000

    Update jenny.py

--- jenny.py
+++ jenny.py
@@ -2,8 +2,11 @@
 import sys
 import socket
 import time
+import getpass
+import signal
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('',23))
 s.listen(10)
 
@@ -12,16 +15,19 @@ print "[+]-------Jenny BETA-------[+]"
 #Sets up telnet sockets
 def tel_con():
     while 1:
-        conn, addr = s.accept()
-        print addr
-        printInfo(addr)
+        try:
+            conn, addr = s.accept()
+            printInfo(addr[0])
+        except KeyboardInterrupt:
+            s.close()
+            sys.exit()
     
 #Compiles and prints
-def printInfo(addr):
-     ip = addr
-     info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip))
+def printInfo(remote_ip):
+     ip = remote_ip
+     info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using USERNAME|PASSWORD")
      print info
-     oFile(info)
+     oFile(info)   
 
 #Creates output file
 def oFile(pInfo):
@@ -29,5 +35,3 @@ def oFile(pInfo):
     fileObject = open("logs.txt", "a")
     fileObject.write(oInfo + "\n")
     fileObject.close()
-     
-tel_con()
