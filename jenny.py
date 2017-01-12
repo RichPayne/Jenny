e39880a704ba8646172b33e71417d15950487085
Richard Payne <richardncpayne@gmail.com>
2017-01-12 11:45:07 +0000

    Update jenny.py
    Removed new lines after each connection and added conditions for no user/pass combo.

--- jenny.py
+++ jenny.py
@@ -22,6 +22,8 @@ def telCon():
             conn.sendall("Password:")
             pw = conn.recv(4096)
             printInfo(addr[0], usr, pw)
+            pwLog(pw)
+            usernameLog(usr)
         except KeyboardInterrupt:
             s.close()
             sys.exit()
@@ -29,18 +31,49 @@ def telCon():
 #Compiles and prints
 def printInfo(remote_ip, username, password):
      ip = remote_ip
-     usr = username
      pw = password
-     info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using " + str(usr) + "|" + str(pw))
+     usr = username
+     if str(usr) == "" and str(pw) == "":
+         info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using no password or username")
+     elif str(usr) == "":
+         usr = username.rsplit()
+         info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using N/A|" + str(usr))
+     elif str(pw) == "":
+         pw = password.rsplit()
+         info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using " + str(pw) + "|N/A")
+     else:
+         usr = username.rsplit()
+         pw = password.rsplit()
+         info = (time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + str(ip) + " using " + str(usr) + "|" + str(pw))
      print info
      oFile(info)   
 
-#Creates output file
+#Creates log output file
 def oFile(pInfo):
     oInfo = pInfo
     fileObject = open("logs.txt", "a")
     fileObject.write(oInfo + "\n")
     fileObject.close()
 
-telCon()
+#produces error log file
+def errorLogs(errorMessage):
+    error = errorMessage
+    fileOnject = open("errorLogs.txt", "a")
+    fileObject.write(error + "\n")
+    fileObject.close()
 
+#produces dedicated passwor log
+def pwLog(password):
+    pw = password
+    fileObject = open("pw.txt", "a")
+    fileObject.write(pw + '\n')
+    fileObject.close()
+    
+#produces dedicated username log
+def usernameLog(username):
+    usr = username
+    fileObject = open("usr.txt", "a")
+    fileObject.write(usr + '\n')
+    fileObject.close()
+    
+telCon()
