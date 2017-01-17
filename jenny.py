739d72098f78d75624248e5abb9540322da009f5
Richard Payne <richardncpayne@gmail.com>
2017-01-17 19:39:13 +0000

    Update jenny.py

--- jenny.py
+++ jenny.py
@@ -2,9 +2,8 @@
 import sys
 import socket
 import time
-import getpass
-import signal
-import xml.etree.ElementTree as ET
+import os
+from xml.dom import minidom
 
 #Create socket and bind
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,6 +11,15 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('',23))
 s.listen(10)
 s.settimeout(10)
+
+#Creates XML structure
+root = minidom.Document()
+xml = root.createElement("root")
+root.appendChild(xml)
+
+#Creates attempt parent
+attemptParent = root.createElement("Attempt")
+xml.appendChild(attemptParent)
 
 print "[+]-------Jenny BETA-------[+]"
 
@@ -51,22 +59,39 @@ def formatStrings(ip, usr, pw):
     elif not pw:
         pw = "No password provided."
         consoleLogger(ip, str(usr[0]), pw)
-        #xmlLogger(ip, str(usr[0]), pw)
+        xmlLogger(ip, str(usr[0]), pw)
     else:
         consoleLogger(ip, str(usr[0]), str(pw[0]))
-        #xmlLogger(ip, str(usr[0]), str(pw[0]))
+        xmlLogger(ip, str(usr[0]), str(pw[0]))
 
 #Logs information to console
 def consoleLogger(ip, usr, pw):
     ip = ip
     usr = str(usr)
     pw = str(pw)
+    
     print(time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + ip + " using " + usr + "|" + pw)
 
 def xmlLogger(ip, usr, pw):
     ip = ip
     usr = str(usr)
     pw = str(pw)
+
+    #Details for attemps created here
+    source = root.createElement("Source")
+    attemptParent.appendChild(source)
+    username = root.createElement("Username")
+    attemptParent.appendChild(username)
+    password = root.createElement("Password")
+    attemptParent.appendChild(password)
+    xml_str = root.toprettyxml(indent="\t")
+    attemptParent.setAttribute('Timestamp', time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]")
+    source.appendChild(root.createTextNode(ip))
+    username.appendChild(root.createTextNode(usr))
+    password.appendChild(root.createTextNode(pw))
+    
+    with open("logs.xml", "w") as xml:
+        xml.write(xml_str)
 
 if __name__ == "__main__":
     recvConnection()
