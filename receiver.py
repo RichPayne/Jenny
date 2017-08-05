9256d3734c955d4ece6dae5b68ae409e38aaab98
Richard Payne <richardncpayne@gmail.com>
2017-08-05 12:43:55 -0700

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -4,14 +4,10 @@ import sys, os, socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('',8989))
-s.listen(10)
-s.settimeout(10)
-
 
 def nodeRecv():
     try:
         conn, addr = s.accept()
-        conn.settimeout(10)
         print conn.recv(65000)
     except socket.error, ex:
         print ex
@@ -33,3 +29,4 @@ def fileLogger(usr, pw):
 if __name__ == "__main__":
     print "Listening for nodes..."
     nodeRecv()
+    
