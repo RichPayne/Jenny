68c383d8b80bce33961496ccfe8ce96f600f9dfe
Richard Payne <richardncpayne@gmail.com>
2017-08-06 22:30:12 +0100

    Add files via upload

---
+++ c2.py
@@ -0,0 +1,27 @@
+import socket, time, os, sys
+#Create socket and bind
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('',8989))
+s.listen(10)
+
+def nodeRecv():
+    while 1:
+        try:
+            try:
+                conn, addr = s.accept()
+                conn.settimeout(1)
+                print conn.recv(65000)
+                conn.close()
+            except socket.error, ex:
+                conn.close()
+                print ex
+        except KeyboardInterrupt:
+            s.close()
+            print "Socket closed.\nStopped."
+            sys.exit()
+    
+if __name__ == "__main__":
+    print "Connected to localhost:8989"
+    print "Listening for nodes..."
+    nodeRecv()
