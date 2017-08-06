ca2103ceb10a73c42f3b7db1637defef83228eae
Richard Payne <richardncpayne@gmail.com>
2017-08-06 20:04:13 +0100

    Delete consoleLogger.py

--- consoleLogger.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def log(ip, usr, pw):
-    ip = ip
-    usr = str(usr)
-    pw = str(pw)
-    
-    print(time.strftime("%d/%m/%Y") + " [" + time.strftime("%H:%M:%S") + "]" + " - " + ip + " using " + usr + "|" + pw)
